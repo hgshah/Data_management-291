6efe0579a99fcce37c3612c974c05d83744cb852
ryankortbeek <rakortbe@ualberta.ca>
2020-11-16 17:54:43 -0700

    added method stubs for all methods that must be implemented

--- db_manager.py
+++ db_manager.py
@@ -46,5 +46,34 @@ class DBManager:
         """
         pass
 
+    def add_post(self, title, body, tags, post_type, user_id, content_license='CC BY-SA 2.5'):
+        # TODO create unique id
+        # TODO set creation date to current date
+        # post_type should be 1 for a question and 2 for an answer
+        score, view_count, answer_count, cmt_count, fav_count = 0, 0, 0, 0, 0
+        pass
+
+    def get_search_results(self, keywords):
+        pass
+
+    def increment_view_count(self, question_id):
+        pass
+
+    def add_answer(self, question_id, body, user_id):
+        pass
+
+    def get_answers(self, question_id):
+        pass
+
+    def check_vote_eligibility(self, post_id, user_id):
+        pass
+
+    def add_vote(self, post_id, user_id):
+        # TODO create unique vote id
+        # TODO set creation date to current date
+        # vote type id should be 2
+        # TODO update score field in Posts
+        pass
+
     def close(self):
         self.client.close()
